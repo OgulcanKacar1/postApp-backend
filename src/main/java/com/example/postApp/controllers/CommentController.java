bdbf280ecc75ca6d37ff33faa776d86f1fb9afff
Ogulcan Kacar <ogulcankacarr@gmail.com>
2025-04-21 21:59:55 +0300

    commant service ve controller olusturuldu; ayni zamanda Comment icin getMapping yapildi.

---
+++ src/main/java/com/example/postApp/controllers/CommentController.java
@@ -0,0 +1,33 @@
+package com.example.postApp.controllers;
+
+
+import com.example.postApp.entities.Comment;
+import com.example.postApp.services.CommentService;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+import java.util.Optional;
+
+@RestController
+@RequestMapping("/comments")
+public class CommentController {
+
+    private CommentService commentService;
+
+    public CommentController(CommentService commentService) {
+        this.commentService = commentService;
+    }
+
+    @GetMapping
+    public List<Comment> getAllComments(@RequestParam Optional<Long> userId,
+                                        @RequestParam Optional<Long> postId) {
+        return commentService.getAllCommentsWithParam(userId, postId);
+
+    }
+
+
+
+}
---
+++ src/main/java/com/example/postApp/services/CommentService.java
@@ -0,0 +1,29 @@
+package com.example.postApp.services;
+
+import com.example.postApp.entities.Comment;
+import com.example.postApp.repos.CommentRepository;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class CommentService {
+    CommentRepository commentRepository;
+
+    public CommentService(CommentRepository commentRepository) {
+        this.commentRepository = commentRepository;
+    }
+
+    public List<Comment> getAllCommentsWithParam(Optional<Long> userId, Optional<Long> postId) {
+        if (userId.isPresent() && postId.isPresent()) {
+            return commentRepository.findByUserIdAndPostId(userId.get(), postId.get());
+        } else if (userId.isPresent()) {
+            return commentRepository.findByUserId(userId.get());
+        } else if (postId.isPresent()) {
+            return commentRepository.findByPostId(postId.get());
+        } else {
+            return commentRepository.findAll();
+        }
+    }
+}
